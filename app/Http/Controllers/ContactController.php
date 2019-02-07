d92dd8635b5a7ea16d85d99ff1b6e83ca590fec2
Neareeves <alethesj@gmail.com>
2019-02-07 09:12:30 +0100

    contact storing function working well

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -3,7 +3,7 @@
 namespace app\Http\Controllers;
 
 use Illuminate\Http\Request;
-// use app\Contact as Contact;
+use app\Contact as Contact;
 
 class ContactController extends Controller
 {
@@ -12,20 +12,44 @@ class ContactController extends Controller
     }
     
     function storeProjet(Request $request) {
+        if ($request->input('name.0') !== NULL) {
+        // $name = $request->input('name.0');
         $contacter = new Contact();
-        $contacter->name=$request->name;
-        $contacter->sujet=$request->sujet;
-        $contacter->mail=$request->mail;
-        $contacter->numero=$request->numero;
-        $contacter->message=$request->message;
+        $contacter->name=$request->input('name.0');
+        $contacter->sujet=$request->input('sujet.0');
+        $contacter->mail=$request->input('mail.0');
+        $contacter->numero=$request->input('numero.0');
+        $contacter->message=$request->input('message.0');
         $contacter->save();
-            // return redirect('/',['ok'=>"message"]);
+       
+            
+        } else if ($request->input('name.1') !== NULL) {
+        $contacter = new Contact();
+        $contacter->name=$request->input('name.1');
+        $contacter->sujet=$request->input('sujet.1');
+        $contacter->mail=$request->input('mail.1');
+        $contacter->numero=$request->input('numero.1');
+        $contacter->message=$request->input('message.1');
+        $contacter->save();
+        }
+        return view('Accueil');
+     
+ // $name = $request->input('name.0');
+ //        $contacter = new Contact();
+ //        $contacter->name=$request->name;
+ //        $contacter->sujet=$request->sujet;
+ //        $contacter->mail=$request->mail;
+ //        $contacter->numero=$request->numero;
+ //        $contacter->message=$request->message;
+ //        $contacter->save();
+ //        echo $contacter;
+//             return redirect('/',['ok'=>"message"]);
 
-// )        [
+//        [
 //     'titre' => request('name'),
 //     'principe' => bcrypt(request('sujet')),
-// ]
-        // echo request('name');
+// ];
+//         echo request('name');
     
     }
 }
